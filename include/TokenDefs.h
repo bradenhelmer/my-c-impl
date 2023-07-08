53c78371830e9bb3f0811b9e8c2e393650ad7cb5
Braden Helmer <bradenhelmeraus@gmail.com>
2023-07-08 10:31:51 -0400

    Added new tokens

--- include/TokenDefs.h
+++ include/TokenDefs.h
@@ -11,33 +11,60 @@
 #define TOKEN_KEYWORD(X) TOKEN(kw_##X)
 #endif
 
-#ifndef TOKEN_KEYWORD_TYPE
-#define TOKEN_KEYWORD_TYPE(X) TOKEN(kwt_##X)
-#endif
-
-#undef EOF
+// Base Tokens
 TOKEN(eof)
-TOKEN(identifier)
-TOKEN(numeric_literal)
 TOKEN(unknown)
+TOKEN(identifier)
+TOKEN(num_const)
+TOKEN(char_const)
+TOKEN(string_literal)
 
+// Organizational tokens
 TOKEN_PUNCTUATOR(o_paren, '(')
 TOKEN_PUNCTUATOR(c_paren, ')')
 TOKEN_PUNCTUATOR(o_brace, '{')
 TOKEN_PUNCTUATOR(c_brace, '}')
 TOKEN_PUNCTUATOR(comma, ',')
+TOKEN_PUNCTUATOR(o_bracket, '[')
+TOKEN_PUNCTUATOR(c_bracket, ']')
 TOKEN_PUNCTUATOR(semi_colon, ';')
-TOKEN_PUNCTUATOR(equals, '=')
-TOKEN_PUNCTUATOR(add, '+')
-TOKEN_PUNCTUATOR(sub, '-')
-TOKEN_PUNCTUATOR(mult, '*')
-TOKEN_PUNCTUATOR(divide, '/')
-TOKEN_PUNCTUATOR(modulo, '%')
+TOKEN_PUNCTUATOR(quote, '"')
+TOKEN_PUNCTUATOR(apost, '\'')
 
-TOKEN_KEYWORD_TYPE(char)
-TOKEN_KEYWORD_TYPE(int)
+// Operators
+TOKEN_PUNCTUATOR(plus, '+')
+TOKEN_PUNCTUATOR(minus, '-')
+TOKEN_PUNCTUATOR(star, '*')
+TOKEN_PUNCTUATOR(slash, '/')
+TOKEN_PUNCTUATOR(percent, '%')
+TOKEN_PUNCTUATOR(equal, '=')
+TOKEN_PUNCTUATOR(plus_equal, "+=")
+TOKEN_PUNCTUATOR(minus_equal, "-=")
+TOKEN_PUNCTUATOR(star_equal, "*=")
+TOKEN_PUNCTUATOR(slash_equal, "/=")
+TOKEN_PUNCTUATOR(plus_plus, "++")
+TOKEN_PUNCTUATOR(minus_minus, "--")
+TOKEN_PUNCTUATOR(pipe_pipe, "||")
+TOKEN_PUNCTUATOR(amp_amp, "&&")
+TOKEN_PUNCTUATOR(bang, '!')
+TOKEN_PUNCTUATOR(less_equal, "<=")
+TOKEN_PUNCTUATOR(less, "<")
+TOKEN_PUNCTUATOR(greater, ">")
+TOKEN_PUNCTUATOR(greater_equal, ">=")
+TOKEN_PUNCTUATOR(equal_equal, "==")
+TOKEN_PUNCTUATOR(bang_equal, "!=")
+
+// Keywords
+TOKEN_KEYWORD(int)
+TOKEN_KEYWORD(char)
+TOKEN_KEYWORD(bool)
+TOKEN_KEYWORD(true)
+TOKEN_KEYWORD(false)
+TOKEN_KEYWORD(if)
+TOKEN_KEYWORD(else)
+TOKEN_KEYWORD(while)
+TOKEN_KEYWORD(for)
 
 #undef TOKEN
 #undef TOKEN_PUNCTUATOR
 #undef TOKEN_KEYWORD
-#undef TOKEN_KEYWORD_TYPE
